45123d099c60308659e46442cb05775b8b3f1e63
alecippolito <ajippolito@live.com>
2024-07-31 15:52:56 -0400

    Changed finder-test.sh for A4P2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,9 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+# username=$(cat conf/username.txt)
+# EDITED FOR A4P2
+username=$(cat etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +34,9 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+# assignment=`cat conf/assignment.txt`
+# EDITED FOR A4P2
+assignment='cat /etc/finder-app/conf/assignment.txt'
 
 if [ $assignment != 'assignment1' ]
 then
@@ -59,6 +63,9 @@ do
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+
+# ADDED FOR A4P2
+echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
